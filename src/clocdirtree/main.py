10ccc76954da74c65ad0e9e23e958c1da20ef7a0
anetczuk <anetczuk@o2.pl>
2024-10-19 13:16:46 +0200

    Add index page

--- src/clocdirtree/main.py
+++ src/clocdirtree/main.py
@@ -51,22 +51,15 @@ def process_cloc(args):
         dirs_list.append(item)
 
     cloc_data_dict = cloc_dirs(dirs_list)
-
-    os.makedirs(out_dir, exist_ok=True)
-    out_file = os.path.join(out_dir, "graph_data.txt")
-    with open(out_file, "w", encoding="utf8") as json_file:
-        json.dump(cloc_data_dict, json_file, allow_nan=False)
-
-    data_dict = {item_key: item_val[0] for item_key, item_val in cloc_data_dict.items()}
-
-    graph = generate_graph(data_dict)
-    out_file = os.path.join(out_dir, "full_graph.png")
-    graph.writePNG(out_file)
-
+    cloc_data_dict = {item_key.removeprefix(run_dir): item_val for item_key, item_val in cloc_data_dict.items() }
+    
     multi_dict = split_to_multi_dict(cloc_data_dict)
+    print("xxx", multi_dict)
     graph_dir = os.path.join(out_dir, "graphs")
     os.makedirs(graph_dir, exist_ok=True)
     generate_from_multidict(multi_dict, graph_dir)
+
+    generate_page_index(out_dir, "graphs/_.html")
 
 
 def generate_from_multidict(multi_dict, out_graph_dir, key_prefix_list=None, key_value=None):
@@ -88,7 +81,10 @@ def generate_from_multidict(multi_dict, out_graph_dir, key_prefix_list=None, key
         graph = generate_graph(graph_dict)
         graph.setName(path_prefix)
 
-        set_node_html_attribs(graph, path_prefix + "_")
+        node_prefix = path_prefix
+        if not node_prefix.endswith("_"):
+            node_prefix += "_"
+        set_node_html_attribs(graph, node_prefix)
         store_graph_to_html(graph, out_graph_dir)
 
         cloc_summary = ""
@@ -112,6 +108,35 @@ def generate_from_multidict(multi_dict, out_graph_dir, key_prefix_list=None, key
         generate_from_multidict(sub_dict, out_graph_dir, sub_list, data_value)
 
 
+def generate_page_index(out_dir, redirect_link):
+    content = f"""\
+<!DOCTYPE HTML>
+<!--
+File was automatically generated using 'cloc-directory-tree' project.
+Project is distributed under the BSD 3-Clause license.
+-->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; url={redirect_link}">
+
+    <title>cloc view</title>
+    <style>
+        body {{  padding: 24;
+                background-color: #bbbbbb;
+             }}
+    </style>
+</head>
+<body>
+    <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+    If you are not redirected automatically, follow this <a href='{redirect_link}'>link to example</a>.
+</body>
+</html>
+"""
+    out_file = os.path.join(out_dir, f"index.html")
+    write_file(out_file, content)
+
+
 def generate_page_content(path_prefix, cloc_summary, out_graph_dir):
     map_path = os.path.join(out_graph_dir, f"{path_prefix}.map")
     map_content = read_file(map_path)
@@ -124,12 +149,13 @@ def generate_page_content(path_prefix, cloc_summary, out_graph_dir):
 """
 
     content = f"""\
-<html>
-<head>
+<!DOCTYPE HTML>
 <!--
 File was automatically generated using 'cloc-directory-tree' project.
 Project is distributed under the BSD 3-Clause license.
 -->
+<html>
+<head>
     <title>cloc view</title>
     <style>
         body {{  padding: 24;
@@ -144,9 +170,14 @@ Project is distributed under the BSD 3-Clause license.
         pre code {{  margin: 0px;
                     padding: 0px;
                  }}
+
+        .backsection {{
+            margin-bottom: 12px;
+        }}
     </style>
 </head>
 <body>
+    <div class="backsection"><a href="../index.html">Back to Index</a></div>
     <div>
 {img_content}
     </div>
